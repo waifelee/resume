88c09f57be976f28545c46f6ba2d35ad429c2d3b
waifelee <liwaifuy@sina.com>
2017-08-31 21:40:09 +0800

    new

---
+++ src/js/components/Pages/Contact/index.js
@@ -0,0 +1,27 @@
+import React,{ Component } from 'react'
+import styles from './index.scss'
+
+class Contact extends Component{
+
+    render(){
+
+        return(
+            <div className="pr">
+                <div className={"bg gray "+styles.bg}></div>
+                <div className="black pa"></div>
+                <section>
+                    <div className={"pa "+ styles.cont}>
+                        <h1>CONTACT <br/>
+                            <b>FOR ME</b></h1>
+                        <ul>
+                            <li>TEL:15986737846</li>
+                            <li>E-MAIL:liwaifuy@sina.com</li>
+                            <li>深圳·宝安区</li>
+                        </ul>
+                    </div>
+                </section>
+            </div>
+        )
+    }
+}
+export default Contact
